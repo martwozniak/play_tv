0c2958db8c3b42f854d9203550562c0bb284a245
Marcin Wozniak <marcindamianwozniak@gmail.com>
2024-12-12 08:15:22 +0100

    extract baseUrl to app_config

---
+++ lib/config/app_config.dart
@@ -0,0 +1,3 @@
+class AppConfig {
+  static const String baseUrl = 'https://playtv-api.parler.com/v3/';
+}
--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:logger/logger.dart';
 import 'package:play_tv/data/network/client/api_client.dart';
 import 'package:play_tv/features/trending/TrendingVideos/data/network/network_mapper.dart';
 import 'package:play_tv/features/trending/TrendingVideos/data/repository/trending_videos_repository.dart';
+import 'package:play_tv/config/app_config.dart';
 
 class DependencyInjection {
   static List<Provider> get providers {
-    final apiClient = ApiClient(baseUrl: 'https://playtv-api.parler.com/v3/');
+    final apiClient = ApiClient(baseUrl: AppConfig.baseUrl);
     final networkMapper = NetworkMapper(logger: Logger());
     final trendingVideosRepository = TrendingVideosRepository(
       apiClient: apiClient,
